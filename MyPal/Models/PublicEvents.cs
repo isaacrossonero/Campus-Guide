6f2b58ab49a2fce4cb1d494d14087721c76b6d22
Philip Paul Grima <philippaul-2002@outlook.com>
2022-04-13 18:13:40 +0200

    Some new updates.

--- MyPal/Models/PublicEvents.cs
+++ MyPal/Models/PublicEvents.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -13,10 +14,13 @@ namespace MyPal.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        [DisplayName("Start time")]
         public DateTime StartTime { get; set; }
+        [DisplayName("End time")]
         public DateTime EndTime { get; set; }
         public int Capacity { get; set; }
         [ForeignKey("Pinpoints")]
+        [DisplayName("Pinpoint ID")]
         public int PinpointId { get; set; }
     }
 }
